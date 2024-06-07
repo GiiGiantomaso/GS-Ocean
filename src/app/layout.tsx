1b59b2dc6e1f49d0d977026b312ea0a95af3b0a5
GiiGiantomaso <rm553369@fiap.com.br>
2024-06-07 00:29:14 -0300

    adicionando icone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,38 @@
-import React, { ReactNode } from "react";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React, { ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import './globals.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Oceanos",
-  description: "Salvar nossos oceanos",
-};
-
+const inter = Inter({ subsets: ['latin'] });
 interface RootLayoutProps {
   children: ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export const metadata = {
+  title: 'Oceanos',
+  description: 'Salvar nossos oceanos',
+};
+const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <html lang="pt-br" style={{ height: "100%" }}>
-      <body className={inter.className} style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-        <Header />
-        <main style={{ flex: 1 }}>
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
+      <html lang="pt-br" style={{ height: '100%' }}>
+        <body className={inter.className} style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Header />
+          <main style={{ flex: 1 }}>
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </>
   );
-}
+};
+
+export default RootLayout;
